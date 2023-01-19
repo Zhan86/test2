3d9eac292b925e5b77ceb3dde273a951d282be8f
UrakovZhanat <zhanaturakov8897@gmail.com>
2023-01-19 08:28:22 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int operand2;
+        char operation;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input operand");
+        int operand1 = scanner.nextInt();
+        do {
+            System.out.println("Input operation");
+            operation = scanner.next().charAt(0);
+
+
+            switch (operation) {
+                case '+':
+                    System.out.println("Input operand");
+                    operand2 = scanner.nextInt();
+                    operand1 += operand2;
+                    System.out.println(operand1);
+                    break;
+                case '-':
+                    System.out.println("Input operand");
+                    operand2 = scanner.nextInt();
+                    operand1 -= operand2;
+                    System.out.println(operand1);
+                    break;
+                case '/':
+                    System.out.println("Input operand");
+                    operand2 = scanner.nextInt();
+                    operand1 /= operand2;
+                    System.out.println(operand1);
+                    break;
+                case '*':
+                    System.out.println("Input operand");
+                    operand2 = scanner.nextInt();
+                    operand1 *= operand2;
+                    System.out.println(operand1);
+                    break;
+                case 'C':
+                    operand1 = 0;
+                    System.out.println(operand1);
+                    System.out.println("Input operand");
+                    operand1 = scanner.nextInt();
+                    break;
+                case 's':
+                    System.out.println("Bye");
+                    break;
+                default:
+                    System.out.println("use +, -, *, /, C - clear, s - quit");
+                    break;
+            }
+        } while (operation != 's');
+
+    }
+}
